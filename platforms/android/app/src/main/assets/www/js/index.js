e6998b340d2b1b7c3682f4b28d34ac4d9be07411
sahmnlama <sahan.lamahewa@gmail.com>
2018-03-16 12:13:55 +0530

    first commit

---
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,10 @@
+function onLoad() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+// device APIs are available
+//
+function onDeviceReady() {
+alert('ready');
+    // Add similar listeners for other events
+}
